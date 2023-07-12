5b8f7480439731547ab3c6ab9517c104a8c8b580
Aura Subhadeep <subhadeepadhikari5@gmail.com>
2023-07-12 12:48:51 +0530

    connect to mongoose

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
 const app = express()
 
+const DataBase = process.env.MONGODB_URL
+
+mongoose.connect(DataBase, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB Atlas database');
+})
+mongoose.connection.on('error', (error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+})
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-    res.send('Server in serving')
+    res.render('home')
 })
-app.listen(3000, () => {
-    console.log('Serving on Port 3000')
+
+const port = process.env.PORT
+app.listen(port, () => {
+    console.log(`The server is running on PORT ${port}`)
 })
