9d2a66e63338eaf051c9487a358bb4a9f264e999
Aura Subhadeep <subhadeepadhikari5@gmail.com>
2023-07-18 15:32:34 +0530

    Catching erros

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
 const ejsMate = require('ejs-mate')
+const catchAsync = require('./utils/catchAsync')
 const method_override = require('method-override')
 const Campground = require('./models/campground')
 
@@ -45,11 +46,11 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 // POST New campground
-app.post('/campgrounds', async(req, res) => {
+app.post('/campgrounds', catchAsync (async(req, res, next) => {
     const campground = new Campground(req.body.campground)
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
 // Show route
 app.get('/campgrounds/:id', async(req, res) => {
@@ -58,23 +59,29 @@ app.get('/campgrounds/:id', async(req, res) => {
 })
 
 // Edit New campground
-app.get('/campgrounds/:id/edit', async(req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync (async(req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', {campground})
-})
+}))
 
 // PUT Update (Edit) campground
-app.put('/campgrounds/:id', async(req, res) => {
+app.put('/campgrounds/:id', catchAsync (async(req, res) => {
     const { id } = req.params
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
 // Delete campground
 app.delete('/campgrounds/:id/', async(req, res) => {
     const { id } = req.params
     await Campground.findByIdAndDelete(id)
     res.redirect('/campgrounds')
+})
+
+
+// Error handler
+app.use((err, req, res, next) =>{
+    res.send("got an error")
 })
 
 // Server Port
