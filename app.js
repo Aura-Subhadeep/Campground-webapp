51db4e0a236caa96499dfe7430c47584e08cd765
Aura Subhadeep <subhadeepadhikari5@gmail.com>
2023-07-19 20:27:31 +0530

    rendered reviews with ratings

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.post('/campgrounds', validateCampground, catchAsync (async(req, res, next) =
 
 // Show route
 app.get('/campgrounds/:id', catchAsync (async(req, res) => {
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id).populate('reviews')
     res.render('campgrounds/show', {campground})
 }))
 
