8bed69503626e3757c9f4194346b7c43b2e2c217
Aura Subhadeep <subhadeepadhikari5@gmail.com>
2023-07-20 11:46:34 +0530

    add: min and max rating value

--- schemas.js
+++ schemas.js
@@ -12,7 +12,7 @@ module.exports.campgroundSchema = joi.object({
 
 module.exports.reviewSchema = joi.object({
     review: joi.object({
-        rating: joi.number().required(),
+        rating: joi.number().required().min(1).max(5),
         body: joi.string().required()
     }).required()
 })
