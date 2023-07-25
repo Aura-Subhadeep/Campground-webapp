39c9af0084d446f2b0b21571b112120bbe0cee54
Aura Subhadeep <subhadeepadhikari5@gmail.com>
2023-07-25 16:03:19 +0530

    add: local login

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const catchAsync = require('../utils/catchAsync')
 const User = require('../models/user')
+const passport = require('passport')
 
 router.get('/register', (req, res) => {
     res.render('users/register')
@@ -13,12 +14,21 @@ router.post('/register', catchAsync(async (req, res) => {
     const user = new User({ email, username})
     const registerdUser = await User.register(user, password)
     console.log(registerdUser)
-    req.flash('success', 'welcomome , you just have signed id')
+    req.flash('success', 'welcomome , you just have registered')
     res.redirect('/campgrounds')
     } catch (e) {
         req.flash('success', e.message)
         res.redirect('register')
     }
 }))
+
+router.get('/login', (req, res) => {
+    res.render('users/login')
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login'}), (req, res) => {
+    req.flash('success', 'welcome back')
+    res.redirect('/campgrounds')
+})
 
 module.exports = router
