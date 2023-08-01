c97223dd7ff54180eebc68a977450b5e6550823e
Aura Subhadeep <subhadeepadhikari5@gmail.com>
2023-08-01 11:58:35 +0530

    revert temp: removed image validation

--- schemas.js
+++ schemas.js
@@ -3,7 +3,7 @@ const joi = require('joi')
 module.exports.campgroundSchema = joi.object({
     campground: joi.object({
         title: joi.string().required(),
-        // image: joi.string().required(),
+        image: joi.required(),
         location: joi.string().required(),
         description: joi.string().required(),
         price: joi.number().required().min(0),
