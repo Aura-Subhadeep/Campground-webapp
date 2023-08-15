c57ece738c934782cec599bcce5a66241b4827ed
Aura Subhadeep <subhadeepadhikari5@gmail.com>
2023-08-15 21:52:55 +0530

    feat: separated frontend and backend

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ mongoose.connection.on('error', (error) => {
 
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, '..', 'frontend', 'views'))
 app.use(express.urlencoded({extended: true}))
 app.use(method_override('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
